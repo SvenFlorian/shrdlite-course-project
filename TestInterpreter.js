bc9cc88c92bb03f63b743d1cf3724a796caefbbb
Florian Minges <florianminges@gmail.com>
2016-05-14 13:00:39 +0200

    Added the test-cases again (accidentally removed them before)

--- TestInterpreter.js
+++ TestInterpreter.js
@@ -3512,6 +3512,54 @@ ExampleWorlds["impossible"] = {
 };
 var allTestCases = [
     { world: "small",
+        utterance: "put a red box on the floor",
+        interpretations: [["ontop(l,floor)"]]
+    },
+    { world: "small",
+        utterance: "put a ball on a box",
+        interpretations: []
+    },
+    { world: "small",
+        utterance: "take an object",
+        interpretations: [["holding(e)", "holding(f)", "holding(g)", "holding(k)", "holding(l)", "holding(m)"]]
+    },
+    { world: "small",
+        utterance: "take a blue object",
+        interpretations: [["holding(g)", "holding(m)"]]
+    },
+    { world: "small",
+        utterance: "take a box",
+        interpretations: [["holding(k)", "holding(l)", "holding(m)"]]
+    },
+    { world: "small",
+        utterance: "put a ball in a box",
+        interpretations: [["inside(e,k)", "inside(e,l)", "inside(f,k)", "inside(f,l)", "inside(f,m)"]]
+    },
+    { world: "small",
+        utterance: "put a ball on a table",
+        interpretations: []
+    },
+    { world: "small",
+        utterance: "put a ball above a table",
+        interpretations: [["above(e,g)", "above(f,g)"]]
+    },
+    { world: "small",
+        utterance: "put a big ball in a small box",
+        interpretations: []
+    },
+    { world: "small",
+        utterance: "put a ball left of a ball",
+        interpretations: [["leftof(e,f)", "leftof(f,e)"]]
+    },
+    { world: "small",
+        utterance: "take a white object beside a blue object",
+        interpretations: [["holding(e)"]]
+    },
+    { world: "small",
+        utterance: "put a white object beside a blue object",
+        interpretations: [["beside(e,g) | beside(e,m)"]]
+    },
+    { world: "small",
         utterance: "put a ball in a box on the floor",
         interpretations: [["inside(e,k)", "inside(f,k)"], ["ontop(f,floor)"]]
     },
